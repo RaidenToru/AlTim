f973722778c59ab47ead2fac880719ed06562f76
Raiden Toru <qwedich@gmail.com>
2019-11-28 12:48:18 +0300

    i dont know

--- Altim/models.py
+++ Altim/models.py
@@ -9,19 +9,25 @@ from django.conf import settings
 class Ticket(models.Model):
     is_two_flight=models.BooleanField()
     ticket_buy_date=models.DateTimeField('Date when flight was bought')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    ticket_price=models.IntegerField('price of ticket',default=0)
 
 
 class Flight(models.Model):
     ticket=models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     flight_name=models.CharField('name of flight',max_length=20)
-    flight_initial_date=models.DateTimeField('date of departure')
-    flight_last_date=models.DateTimeField('date of arrival')
+    flight_initial_date=models.DateField('date of departure',null=True,blank=True)
+    flight_last_date=models.DateField('date of arrival',null=True,blank=True)
+    flight_initial_time=models.TimeField('time of departure',null=True,blank=True)
+    flight_last_time=models.TimeField('time of arrival',null=True,blank=True)
     flight_initial_place=models.CharField('place of departure',max_length=50)
     flight_last_place=models.CharField('place of arrival',max_length=50)
     flight_rating=models.FloatField('rating of flight')
-    flight_price=models.IntegerField('price of flight')
     plane_name=models.CharField('name of plane',max_length=20)
     flight_aircompany=models.ImageField( upload_to='user_avas/')
+    aircompany_name=models.CharField(max_length=20)
+    flight_time=models.CharField(max_length=20)
 
     def __str__(self):
         return self.flight_name
