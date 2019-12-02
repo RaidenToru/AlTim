db1f09bc7a6896f6558f7e14562e2285701a49d8
Raiden Toru <qwedich@gmail.com>
2019-12-02 11:37:49 +0300

    qq

--- Altim/urls.py
+++ Altim/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.index, name='index'),
     path('map/',views.map, name='map'),
-    path('personal/',views.personal, name='personal')
+    path('personal/<int:user_id>/',views.personal, name='personal'),
+    path('personal/<user_id>/results/',views.get_search_result, name='get_search_result')
 ]
--- Altim/views.py
+++ Altim/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render, redirect
 from .models import Ticket,Flight,SimpleUser
 from Altim.forms import UserRegistrationForm
 
+
 def index(request):
     return render(request,'index.html')
 
-def personal(request):
-    user_id=1
+def personal(request, user_id):
     tickets=Ticket.objects.filter(user=SimpleUser.objects.get(pk=user_id))
     flights=Flight.objects.filter(user=SimpleUser.objects.get(pk=user_id))
 
@@ -14,3 +14,13 @@ def personal(request):
 
 def map(request):
     return render(request, 'map.html')
+
+def get_search_result(request,user_id):
+    try:
+        if request.method=="POST":
+            query=request.POST.get('q')
+            flights_result=Flight.objects.filter(flight_name__icontains=query, user=SimpleUser.objects.get(pk=user_id))
+            tickets=Ticket.objects.filter(user=SimpleUser.objects.get(pk=user_id))
+            return render(request,'personalSearch.html',{'flights_result':flights_result,'tickets':tickets})
+    except:
+        return render(request,'personalSearch.html')
